5c784f2fbb8f1c54d6e58c7eab9aea6a075fa308
Kundan <kundan kamble>
2022-08-10 01:51:50 -0800

    Find Maximum String program

--- GenericProblems/MaxFloatNumbers.cs
+++ GenericProblems/MaxFloatNumbers.cs
@@ -6,34 +6,33 @@ using System.Threading.Tasks;
 
 namespace GenericProblems
 {
-    public class MaxFloatNumbers
+    internal class MaxFloatNumbers
     {
-            public float FloatNumber(float num_1, float num_2, float num_3) //Parameterized Method for Comparing Numbers And Find Maximum Float Number
+        public float FloatNumber(float num_1, float num_2, float num_3)//Method for Comparing Numbers And Find Maximum Float Number
+        {
+            Console.WriteLine("Three Float numbers are : num1 = {0} , num2 = {1} , num3 = {2} ", num_1, num_2, num_3);
+
+            if (num_1.CompareTo(num_2) > 0 && num_1.CompareTo(num_3) > 0)
             {
-                Console.WriteLine("Three Float numbers are : num1 = {0} , num2 = {1} , num3 = {2} ", num_1, num_2, num_3);
-
-                if (num_1.CompareTo(num_2) > 0 && num_1.CompareTo(num_3) > 0)
-                {
-                    Console.Write("Max Float Number = ");
-                    return num_1;
-                }
-                else if (num_2.CompareTo(num_1) > 0 && num_2.CompareTo(num_3) > 0)
-                {
-                    Console.Write("Max Float Number = ");
-                    return num_2;//Console.WriteLine("{0} is Max number ", num2);
-                }
-                else if (num_3.CompareTo(num_1) > 0 && num_3.CompareTo(num_2) > 0)
-                {
-                    Console.Write("Max Float Number = ");
-                    return num_3;//Console.WriteLine("{0} is Max number ", num3);
-                }
-                else
-                {
-                    Console.WriteLine("all are same ");
-                }
-                return 0;
+                Console.Write("Max Float Number = ");
+                return num_1;
             }
+            else if (num_2.CompareTo(num_1) > 0 && num_2.CompareTo(num_3) > 0)
+            {
+                Console.Write("Max Float Number = ");
+                return num_2;//Console.WriteLine("{0} is Max number ", num2);
+            }
+            else if (num_3.CompareTo(num_1) > 0 && num_3.CompareTo(num_2) > 0)
+            {
+                Console.Write("Max Float Number = ");
+                return num_3;//Console.WriteLine("{0} is Max number ", num3);
+            }
+            else
+            {
+                Console.WriteLine("all are same ");
+            }
+            return 0;
+        }
+
     }
-
 }
-
--- GenericProblems/Program.cs
+++ GenericProblems/Program.cs
@@ -1,11 +1,21 @@
-﻿namespace GenericProblems
+﻿using System;
+
+namespace GenericProblems
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            MaxFloatNumbers maxintnum = new MaxFloatNumbers();
-            Console.WriteLine(maxintnum.FloatNumber(10.5f, 80.6f, 10.5f));
+            MaxIntNumbers maxintnum = new MaxIntNumbers();
+            Console.WriteLine(maxintnum.NumberMax(10, 80, 10));
+            Console.WriteLine("-------------------------");
+
+            MaxFloatNumbers maxfloatnum = new MaxFloatNumbers();
+            Console.WriteLine(maxfloatnum.FloatNumber(90.4f, 50.6f, 70.7f));
+            Console.WriteLine("-------------------------");
+
+            MaxString maxString = new MaxString();
+            Console.WriteLine(maxString.largeString("Apple", "Peach", "Banana"));
         }
     }
 }
