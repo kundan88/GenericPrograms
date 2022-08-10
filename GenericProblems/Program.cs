4cf8e3c95bf32c4c5b1a08e898be80b5ce20a40f
Kundan <kundan kamble>
2022-08-10 01:27:31 -0800

    Find Maximum int number program

---
+++ GenericProblems/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MaxIntNumbers maxintnum = new MaxIntNumbers();
+            Console.WriteLine(maxintnum.NumberMax(10, 80, 10));
+        }
+    }
+}
