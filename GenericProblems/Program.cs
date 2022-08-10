1079862a89a8c36d77be5eee4f526b61522f1cf1
Kundan <kundan kamble>
2022-08-10 02:08:29 -0800

    Extend max method with three param program

--- GenericProblems/Program.cs
+++ GenericProblems/Program.cs
@@ -6,16 +6,17 @@ namespace GenericProblems
     {
         public static void Main(string[] args)
         {
-            MaxIntNumbers maxintnum = new MaxIntNumbers();
-            Console.WriteLine(maxintnum.NumberMax(10, 80, 10));
-            Console.WriteLine("-------------------------");
+            int[] intArray = { 10, 44, 33, 88, 444, 11, 8, };
+            float[] floatArray = { 66.5f, 15.33f, 33.44f, 15.55f, 111.2f };
+            string[] stringArray = { "Apple", "Peach", "Banana", "sumit", "pawan", "kundan" };
 
-            MaxFloatNumbers maxfloatnum = new MaxFloatNumbers();
-            Console.WriteLine(maxfloatnum.FloatNumber(90.4f, 50.6f, 70.7f));
-            Console.WriteLine("-------------------------");
-
-            MaxString maxString = new MaxString();
-            Console.WriteLine(maxString.largeString("Apple", "Peach", "Banana"));
+            MaxGeneric<int> intValue = new MaxGeneric<int>(intArray);
+            Console.WriteLine(intValue.testMaximum());
+            MaxGeneric<float> floatValue = new MaxGeneric<float>(floatArray);
+            Console.WriteLine(floatValue.testMaximum());
+            MaxGeneric<string> strValue = new MaxGeneric<string>(stringArray);
+            Console.WriteLine(strValue.testMaximum());
         }
+
     }
 }
