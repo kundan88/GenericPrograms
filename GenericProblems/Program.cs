075b09e0c660632e4e5ebed07786d8370a29189c
Kundan <kundan kamble>
2022-08-10 07:46:04 -0800

    Find Max Print Using Generic method program

---
+++ GenericProblems/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] intArray = { 10, 44, 33, 88, 444, 11, 8, };
+            float[] floatArray = { 66.5f, 15.33f, 33.44f, 15.55f, 111.2f };
+            string[] stringArray = { "Apple", "Peach", "Banana", "suraj", "karan", "kundan" };
+
+            MaxGeneric<int> intValue = new MaxGeneric<int>(intArray);
+            intValue.printMax();
+            MaxGeneric<float> floatValue = new MaxGeneric<float>(floatArray);
+            floatValue.printMax();
+            MaxGeneric<string> strValue = new MaxGeneric<string>(stringArray);
+            strValue.printMax();
+        }
+
+    }
+}
